2e10e3a0877d325a938cb98bb2fd6a10f803c00c
‪Mohamed Foullane‬‏ <142094528+Foullane-Mohamed@users.noreply.github.com>
2024-11-27 01:29:38 +0100

    updite style cards et ajouter tilte

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+let inputNom = document.querySelector('.input-nom')
+let inputPhoto = document.querySelector('.input-photo')
+let inputDrapeau = document.querySelector('.input-drapeau')
+let inputClub = document.querySelector('.input-club')
+let inputLogoNationalite = document.querySelector('.input-logo-nationalite')
+let inputLogoClub = document.querySelector('.input-logo-club')
+let selectPosition = document.querySelector('.select-position')
+let inputTir = document.querySelector('.input-tir')
+let inputPasse = document.querySelector('.input-passe')
+let inputDribble = document.querySelector('.input-dribble')
+let inputDefense = document.querySelector('.input-defense')
+let inputPlongee = document.querySelector('.input-plongee')
+let inputManiement = document.querySelector('.input-maniement')
+let inputCoupPied = document.querySelector('.input-coup-pied')
+let inputReflexes = document.querySelector('.input-reflexes')
+let inputVitesse = document.querySelector('.input-vitesse')
+let inputPlacement = document.querySelector('.input-placement')
+let btnEnter = document.querySelector('.btn-enter')
+
+
+
+btnEnter.addEventListener('click',()=>{
+  console.log("hello");
+  
+})
